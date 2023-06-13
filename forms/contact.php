03b2b972b19f41af1d496aae3caebdc2066bc1a3
Kartikey Mishra <76617485+KartikeyMish@users.noreply.github.com>
2023-06-13 23:35:27 +0530

    Updates made

--- forms/contact.php
+++ forms/contact.php
@@ -1,98 +1,15 @@
 <?php
+//get data from form  
 
-// Include packages and files for PHPMailer and SMTP protocol
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer-master/src/Exception.php';
-require 'PHPMailer-master/src/PHPMailer.php';
-require 'PHPMailer-master/src/SMTP.php';
-
-// Initialize PHP mailer, configure to use SMTP protocol and add credentials
-
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Mailer = "smtp";
-
-$mail->SMTPDebug  = 0;
-$mail->SMTPAuth   = TRUE;
-$mail->SMTPSecure = "ssl";
-$mail->Port       = 465;
-$mail->Host       = "smtp.gmail.com";
-$mail->Username   = "eclassroom1999@gmail.com";
-$mail->Password   = "gxptrkpjfvmrqbce";
-
-
-$success = "";
-$error = "";
-$name = $message = $email = "";
-$errors = array('name' => '', 'email' => '', 'message' => '');
-
-if (isset($_POST["submit"])) {
-    if (empty(trim($_POST["name"]))) {
-        $errors['name'] = "Your name is required";
-    } else {
-        $name = SanitizeString($_POST["name"]);
-        if (!preg_match('/^[a-zA-Z\s]{6,50}$/', $name)) {
-            $errors['name'] = "Only letters and spaces allowed";
-        }
-    }
-
-    if (empty(trim($_POST["email"]))) {
-        $errors["email"] = "Your email is required";
-    } else {
-        $email = SanitizeString($_POST["email"]);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors["email"] = "Pls give a proper email address";
-        }
-    }
-
-    if (empty(trim($_POST["message"]))) {
-        $errors["message"] = "Please type your message";
-    } else {
-        $message = SanitizeString($_POST["message"]);
-        if (!preg_match("/^[a-zA-Z\d\s]+$/", $message)) {
-            $errors["message"] = "Only letters, spaces and maybe numbers allowed";
-        }
-    }
-
-    if (array_filter($errors)) {
-    } else {
-        try {
-
-            $mail->setFrom('eclassroom1999@gmail', 'Anirudha B Shetty');
-
-            $mail->addAddress($email, $name);
-
-            $mail->Subject = 'Build a contact form with PHP';
-
-            $mail->Body = $message;
-
-            // send mail
-
-            $mail->send();
-
-            // empty users input
-
-            $name = $message = $email = "";
-
-            $success = "Message sent successfully";
-        } catch (Exception $e) {
-
-            // echo $e->errorMessage(); use for testing & debugging purposes
-            $error = "Sorry message could not send, try again";
-        } catch (Exception $e) {
-
-            // echo $e->getMessage(); use for testing & debugging purposes
-            $error = "Sorry message could not send, try again";
-        }
-    }
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "kartikeymishra626@gmail.com";
+$subject = $_POST['subject'];
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@katikey.portfolio.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
-
-function SanitizeString($var)
-{
-    $var = strip_tags($var);
-    $var = htmlentities($var);
-    return stripslashes($var);
-}
+//redirect
+?>
